cd600fb8bbcd8a014582dac0de5afeb1a68473d6
Rene <renega1238@gmail.com>
2022-10-06 19:38:11 -0500

    Cd like C implementation

---
+++ CdC/README.md
@@ -0,0 +1,6 @@
+# Cd like C implementation 
+
+## Overview
+cd command implementation in C - I intend to use 'chdir' but I am not sure if this is a little bit silly
+
+I will just print current working directory to test this
---
+++ CdC/cdc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h> 
+#include <unistd.h> 
+
+int main(int argc, char *argv[argc + 1])
+{
+	assert(argc == 2 && "Wrong usage - ./cdc <dir>"); 
+
+	char cwd[100]; 
+
+	getcwd(cwd, sizeof(cwd)); 
+	printf("Current dir -> %s\n", cwd); 
+
+	if (chdir(argv[1]))
+		goto err; 
+
+	getcwd(cwd, sizeof(cwd)); 
+	printf("Current dir -> %s\n", cwd); 
+	
+	return EXIT_SUCCESS; 
+
+err: 
+	printf("Directory < %s > does not exist\n", argv[1]); 
+	return EXIT_FAILURE; 
+}
+
+
